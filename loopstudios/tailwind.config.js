8b53cbf716df362fce05f75e9aad50f9b1da16b5
Cleomir <cleomir.mg@hotmail.com>
2023-06-06 18:50:28 -0300

    Add creations section

--- loopstudios/tailwind.config.js
+++ loopstudios/tailwind.config.js
@@ -9,15 +9,12 @@ module.exports = {
       xl: "1440px",
     },
     extend: {
-      // colors: {
-      //   strongCyan: "hsl(171, 66%, 44%)",
-      //   lightBlue: "hsl(233, 100%, 69%)",
-      //   darkGrayishBlue: "hsl(210, 10%, 33%)",
-      //   grayishBlue: "hsl(201, 11%, 66%)",
-      // },
       fontFamily: {
         sans: ["Josefin Sans", "sans-serif"],
         alata: ["Alata"],
+      },
+      letterSpacing: {
+        widest: ".3em",
       },
     },
   },
