1530f8362e908ffaa79c3674caacfdec9a3a375e
Cleomir <cleomir.mg@hotmail.com>
2023-06-10 22:19:47 -0300

    Add mobile menu

--- bookmark-manager/src/script.js
+++ bookmark-manager/src/script.js
@@ -1,5 +1,21 @@
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const logo = document.getElementById("logo");
 const tabs = document.querySelectorAll(".tab");
 const panels = document.querySelectorAll(".panel");
+
+// hamburger button
+btn.addEventListener("click", () => {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+
+  if (menu.classList.contains("flex")) {
+    logo.setAttribute("src", "images/logo-bookmark-footer.svg");
+  } else {
+    logo.setAttribute("src", "images/logo-bookmark.svg");
+  }
+});
 
 tabs.forEach((tab) =>
   tab.addEventListener("click", (event) => {
